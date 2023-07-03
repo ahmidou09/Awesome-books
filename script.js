b5bfd020e953e63e03f3b2c44b1a632a1bab75d1
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-07-03 13:09:01 -0300

    funcion remove

--- script.js
+++ script.js
@@ -7,42 +7,87 @@ const listBooks = document.querySelector('.listBooks');
 const books = [];
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = titleInput.value;
-  const author = authorInput.value;
+    e.preventDefault();
+    const title = titleInput.value;
+    const author = authorInput.value;
 
-  if (title && author) {
-    const itemBook = {
-      title,
-      author,
-    };
+    if (title && author) {
+        const itemBook = {
+            title,
+            author,
+        };
 
-    books.push(itemBook);
+        books.push(itemBook);
 
-    titleInput.value = '';
-    authorInput.value = '';
+        titleInput.value = '';
+        authorInput.value = '';
 
-    listBooks.innerHTML = '';
+        listBooks.innerHTML = '';
 
+        books.forEach((book, index) => {
+            const bookDiv = document.createElement('ul');
+            bookDiv.classList.add('book');
+
+            const spanTitle = document.createElement('li');
+            spanTitle.innerText = `Title: ${book.title}`;
+
+            const spanAuthor = document.createElement('li');
+            spanAuthor.innerText = `Author: ${book.author}`;
+
+            const removeButton = document.createElement('button');
+            removeButton.innerHTML = 'remove';
+            removeButton.classList.add('removeBook');
+
+            removeButton.addEventListener("click", () => {
+                removeBook(index);
+            });
+
+            bookDiv.appendChild(spanTitle);
+            bookDiv.appendChild(spanAuthor);
+            bookDiv.appendChild(removeButton);
+
+            listBooks.appendChild(bookDiv);
+        });
+    }
+});
+
+function removeBook(index) {
+    let arr = books.filter((book, i) => {
+      console.log("index", index);
+      console.log("i", i);
+  
+      if (index !== i) return book;
+    });
+  
+    books = arr;
+  
+    localStorage.setItem("books", JSON.stringify(books));
+  
+    listBooks.innerHTML = "";
+  
     books.forEach((book, index) => {
-      const bookDiv = document.createElement('ul');
-      bookDiv.classList.add('book');
-
-      const spanTitle = document.createElement('li');
+      const bookDiv = document.createElement("ul");
+      bookDiv.classList.add("book");
+  
+      const spanTitle = document.createElement("li");
       spanTitle.innerText = `Title: ${book.title}`;
-
-      const spanAuthor = document.createElement('li');
+  
+      const spanAuthor = document.createElement("li");
       spanAuthor.innerText = `Author: ${book.author}`;
-
-      const removeButton = document.createElement('button');
-      removeButton.innerHTML = 'remove';
-      removeButton.classList.add('removeBook');
-
+  
+      const removeButton = document.createElement("button");
+      removeButton.innerHTML = "remove";
+      removeButton.classList.add("removeBook");
+  
+      removeButton.addEventListener("click", () => {
+        removeBook(index);
+      });
+  
       bookDiv.appendChild(spanTitle);
       bookDiv.appendChild(spanAuthor);
       bookDiv.appendChild(removeButton);
-
+  
       listBooks.appendChild(bookDiv);
     });
   }
-});
+  
