db871cff42021876ba7363a1acb728b9950ea017
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-07-04 15:58:11 +0100

    Use JavaScript classes instead of objects and arrays

--- script.js
+++ script.js
@@ -1,96 +1,19 @@
-const form = document.querySelector('.form');
-const titleInput = document.querySelector('.titleInput');
-const authorInput = document.querySelector('.authorInput');
-const listBooks = document.querySelector('.listBooks');
+class BookList {
+  constructor() {
+    this.form = document.querySelector('.form');
+    this.titleInput = document.querySelector('.titleInput');
+    this.authorInput = document.querySelector('.authorInput');
+    this.listBooks = document.querySelector('.listBooks');
 
-let books = JSON.parse(localStorage.getItem('books')) || [];
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
 
-listBooks.innerHTML = '';
+    this.renderBooks();
+    this.form.addEventListener('submit', this.handleFormSubmit);
+  }
 
-function removeBook(index) {
-  const arr = books.filter((book, i) => {
-    if (index !== i) return book;
-    return null;
-  });
-
-  books = arr;
-
-  localStorage.setItem('books', JSON.stringify(books));
-
-  listBooks.innerHTML = '';
-
-  books.forEach((book, index) => {
-    const bookDiv = document.createElement('ul');
-    bookDiv.classList.add('book');
-
-    const spanTitle = document.createElement('li');
-    spanTitle.innerText = `Title: ${book.title}`;
-
-    const spanAuthor = document.createElement('li');
-    spanAuthor.innerText = `Author: ${book.author}`;
-
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = 'remove';
-    removeButton.classList.add('removeBook');
-
-    removeButton.addEventListener('click', () => {
-      removeBook(index);
-    });
-
-    bookDiv.appendChild(spanTitle);
-    bookDiv.appendChild(spanAuthor);
-    bookDiv.appendChild(removeButton);
-
-    listBooks.appendChild(bookDiv);
-  });
-}
-
-books.forEach((book, index) => {
-  const bookDiv = document.createElement('ul');
-  bookDiv.classList.add('book');
-
-  const spanTitle = document.createElement('li');
-  spanTitle.innerText = `Title: ${book.title}`;
-
-  const spanAuthor = document.createElement('li');
-  spanAuthor.innerText = `Author: ${book.author}`;
-
-  const removeButton = document.createElement('button');
-  removeButton.innerHTML = 'remove';
-  removeButton.classList.add('removeBook');
-
-  removeButton.addEventListener('click', () => {
-    removeBook(index);
-  });
-
-  bookDiv.appendChild(spanTitle);
-  bookDiv.appendChild(spanAuthor);
-  bookDiv.appendChild(removeButton);
-
-  listBooks.appendChild(bookDiv);
-});
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = titleInput.value;
-  const author = authorInput.value;
-
-  if (title && author) {
-    const itemBook = {
-      title,
-      author,
-    };
-
-    books.push(itemBook);
-
-    localStorage.setItem('books', JSON.stringify(books));
-
-    titleInput.value = '';
-    authorInput.value = '';
-
-    listBooks.innerHTML = '';
-
-    books.forEach((book, index) => {
+  renderBooks = () => {
+    this.listBooks.innerHTML = '';
+    this.books.forEach((book, index) => {
       const bookDiv = document.createElement('ul');
       bookDiv.classList.add('book');
 
@@ -105,14 +28,47 @@ form.addEventListener('submit', (e) => {
       removeButton.classList.add('removeBook');
 
       removeButton.addEventListener('click', () => {
-        removeBook(index);
+        this.removeBook(index);
       });
 
       bookDiv.appendChild(spanTitle);
       bookDiv.appendChild(spanAuthor);
       bookDiv.appendChild(removeButton);
 
-      listBooks.appendChild(bookDiv);
+      this.listBooks.appendChild(bookDiv);
     });
-  }
-});
+  };
+
+  removeBook = (index) => {
+    this.books = this.books.filter((book, i) => index !== i);
+
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.renderBooks();
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const title = this.titleInput.value;
+    const author = this.authorInput.value;
+
+    if (title && author) {
+      const itemBook = {
+        title,
+        author,
+      };
+
+      this.books.push(itemBook);
+
+      localStorage.setItem('books', JSON.stringify(this.books));
+
+      this.titleInput.value = '';
+      this.authorInput.value = '';
+
+      this.renderBooks();
+    }
+  };
+}
+
+const book = new BookList();
+
+console.log(book);
